e911afd68d5f31b9ea489d08afdf07074e22903a
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 20:36:51 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cerebrocore/main.go
@@ -0,0 +1,23 @@
+// cmd/cerebrocore/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"cerebrocore/internal/cerebrocore"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := cerebrocore.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
